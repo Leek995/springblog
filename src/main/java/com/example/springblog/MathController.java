177ee82e263ffd49f737ca90a51d0c595a2512c2
Malik <willis.malik.r@gmail.com>
2022-11-17 15:52:47 -0600

    MathController complete

--- src/main/java/com/example/springblog/MathController.java
+++ src/main/java/com/example/springblog/MathController.java
@@ -1,8 +1,16 @@
 package com.example.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MathController {
-
+    @GetMapping("/add/{three}/and/{four}")
+    @ResponseBody
+    public String addition(@PathVariable int three, @PathVariable int four){
+        int addition = three + four;
+        return "sum of " + addition;
+    }
 }
