7467c7382d21553a286dc35313765a3cb79fd706
Malik <willis.malik.r@gmail.com>
2022-11-17 16:08:45 -0600

    Post Controller skeleton arranged

---
+++ src/main/java/com/example/springblog/PostController.java
@@ -0,0 +1,35 @@
+package com.example.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @GetMapping("/post")
+    @ResponseBody
+    public String post(){
+        return "posts index page";
+    }
+    @GetMapping("/post/{id}")
+    @ResponseBody
+    public String postId(@PathVariable int id){
+        return "view an individual post " + id;
+    }
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String postCreate(){
+        return "view the form for creating a post";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String postCreateSubmit(){
+        return "create new post";
+    }
+
+
+
+
+}
